29ab0b6b253e81358c035c13729442f8f2082ff2
Kevin C. Escobedo <escobedo001@gmail.com>
2019-07-15 15:11:00 -0700

    Fixed 0 base bug

--- converter_ui.py
+++ converter_ui.py
@@ -1,8 +1,11 @@
+#Author: Kevin C. Escobedo
+#Email: escobedo001@gmail.com
 import num_converter
 import tkinter
 
 class Converter:
     def __init__(self):
+        '''Sets up the converter GUI'''
         self.root_window = tkinter.Tk()
         self.root_window.geometry("300x150")
         self.root_window.resizable(0, 0)
@@ -13,33 +16,45 @@ class Converter:
         self.result = tkinter.StringVar()
         self.converted_result = ""
 
-    def get_first_number(self):
+    def get_number(self):
+        '''Gets the original number'''
         try:
             return int(self.number.get())
         except:
             self.result.set("Invalid Input for Number")
 
-    def get_first_base(self):
+    def get_base(self, original = 0):
+        '''Gets the base'''
         try:
-            return int(self.base.get())
-        except:
-            self.result.set("Invalid Input for Base")
+            if original == 1:
+                base = int(self.base.get())
+            else:
+                base = int(self.new_base.get())
 
-    def get_second_base(self):
-        try:
-            return int(self.new_base.get())
-        except:
-            self.result.set("Invalid Input for New Base")
+            if base == 0:
+                raise ValueError
+
+            return base
+
+        except ValueError:
+            if original == 1:
+                self.result.set("Invalid Input for Base")
+            else:
+                self.result.set("Invalid Input for New Base")
         
 
     def convert(self):
-        original_number = self.get_first_number()
-        original_base = self.get_first_base()
-        other_base = self.get_second_base()
-        self.converted_result = num_converter.convert_to_base(num_converter.convert_to_decimal(original_number, original_base), other_base)
-        self.result.set("Result: {}".format(self.converted_result))
+        '''Gets the necessary numbers, passes them into converter logic'''
+        original_number = self.get_number()
+        original_base = self.get_base(1)
+        other_base = self.get_base(0)
+
+        if original_number and original_base and other_base:
+            self.converted_result = num_converter.convert_to_base(num_converter.convert_to_decimal(original_number, original_base), other_base)
+            self.result.set("Result: {}".format(self.converted_result))
 
     def run(self):
+        '''Shows where to enter numbers, shows the result'''
         tkinter.Label(self.root_window, text = "Number").grid(row=0, column = 1, sticky = tkinter.NSEW)
         tkinter.Label(self.root_window, text = "Base").grid(row=1, column = 1, sticky = tkinter.NSEW)
         tkinter.Label(self.root_window, text = "New Base").grid(row=2, column = 1, sticky = tkinter.NSEW)
@@ -55,8 +70,6 @@ class Converter:
         button.grid(row = 3, column = 2, sticky = tkinter.NSEW)
 
         self.root_window.mainloop()
-
-        
 
 
 
