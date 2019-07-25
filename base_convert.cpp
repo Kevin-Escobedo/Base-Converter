bdc209f4e226fe138548c9abb04245ba0d579788
Kevin C. Escobedo <escobedo001@gmail.com>
2019-07-25 14:26:06 -0700

    Added convert_to_base function

--- base_convert.cpp
+++ base_convert.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <stdlib.h>
 using namespace std;
 
 int to_power(int base, int exponent)
@@ -13,7 +14,7 @@ int to_power(int base, int exponent)
 	return value;
 }
 
-int convert_to_decimal(int num = 0, int base = 10)
+int convert_to_decimal(int num = 0, int base = 10) //Can't convert to hex
 {
 	string nums = to_string(num);
 	int value = 0;
@@ -35,8 +36,57 @@ int convert_to_decimal(int num = 0, int base = 10)
 	return value;
 }
 
+string flip(string s)
+{
+	string flipped;
+	int i;
+	int j = s.length() - 1;
+	for(i = 0; i < s.length(); i++)
+	{
+		flipped[i] = s[j];
+		j--;
+	}
+	return flipped;
+}
+
+int convert_to_base(int number = 0, int base = 10, bool steps = false) //Hex not supported
+{
+	string value;
+	int mod = 0;
+	int my_div = 0;
+
+	while((my_div != 1) or (my_div != 0))
+	{
+		mod = number % base;
+		value += to_string(mod);
+		if(steps)
+		{
+			cout<<number<<" mod "<<base<<" = "<<mod<<endl;
+		}
+		my_div = div(number, base).quot;
+		if(steps)
+		{
+			cout<<number<<" div "<<base<<" = "<<my_div<<endl;
+		}
+
+		number = my_div;
+
+		if(my_div == 0)
+		{
+			return stoi(flip(value));
+		}
+
+		else if(my_div == 1)
+		{
+			value += to_string(my_div);
+			return stoi(flip(value));
+		}
+	}
+	return stoi(flip(value));
+}
+
 int main()
 {
-	int a = convert_to_decimal(101, 2);
+	int a = convert_to_base(convert_to_decimal(111010, 2), 3);
 	cout<<a<<endl;
 }
